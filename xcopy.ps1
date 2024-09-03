d5b668390b07b9a8b9682101b2f63ea1cfce3763
José Antonio Estevan <1043626+jaestevan@users.noreply.github.com>
2024-09-04 00:26:07 +0200

    Small script for quick testing changes

--- xcopy.ps1
+++ xcopy.ps1
@@ -1,9 +1,9 @@
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/copy-item?view=powershell-7.4
 
-Copy-Item "config" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -force -recurse -verbose
-Copy-Item "data" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -force -recurse -verbose
-#Copy-Item "icon" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -force -recurse -verbose
-Copy-Item "locale" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -force -recurse -verbose
-Copy-Item "main" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -force -recurse -verbose
-#Copy-Item "ref" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -force -recurse -verbose
-Copy-Item "SmexyMats.toc" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -force -verbose
+Copy-Item "config" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -Force -Recurse -Verbose
+Copy-Item "data"   "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -Force -Recurse -Verbose
+#Copy-Item "icon"   "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -Force -Recurse -Verbose
+Copy-Item "locale" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -Force -Recurse -Verbose
+Copy-Item "main"   "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -Force -Recurse -Verbose
+#Copy-Item "ref"    "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -Force -Recurse -Verbose
+Copy-Item "SmexyMats.toc" "E:\JUEGOS\World of Warcraft\_retail_\Interface\AddOns\SmexyMats\" -Force -Verbose
